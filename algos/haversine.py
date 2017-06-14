80a12763fd5cbf0078626aecb170e8d2850eeb97
Rui <rz187@scarletmail.rutgers.edu>
2017-06-14 17:17:30 -0400

    more haversine stuff

--- algos/haversine.py
+++ algos/haversine.py
@@ -14,3 +14,11 @@ def haversine(latlng1, latlng2): # ((double,double), (double,double)) -> double
     subeqns = [math.sin(delta_phi / 2) ** 2, math.cos(phi1) * math.cos(phi2) * math.sin(delta_lambda / 2) ** 2]
     inner_sum = subeqns[0] + subeqns[1]
     return EARTH_RADIUS * 2 * math.atan2(math.sqrt(inner_sum), math.sqrt(1-inner_sum))
+
+def get_bounding_box(latlng, edge): # ((double, double), double) -> ((double, double), (double, double))
+    pass
+
+testLats = ((29.425856, -98.561619), (40.489427, -74.451804), (37.770383, -122.466525), (48.907846, -95.317017)) # Texas, NJ, California, Minnesotta
+d = 0.0075 # 1 km offset
+for testLat in testLats:
+    print(haversine(testLat, (testLat[0]+d, testLat[1]+d)))
